99ce11bf0792b3fc063da72861135d5925fda901
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-04-08 09:56:18 +0600

    CO

---
+++ comparisonOperator.js
@@ -0,0 +1,16 @@
+let a='10';
+let b=10;
+let c=15;
+
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!=b);
+console.log(a>c);
+
+if(a>b){
+    console.log('a is gtreater than b');
+}
+else{
+    console.log('a is less than b');
+}
