7a88a8521d7f9fdb23b0c8f608dc291d6cd43bac
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-04-08 09:33:25 +0600

    variables

---
+++ variable.js
@@ -0,0 +1,15 @@
+
+
+console.log('hello world');
+var x = 5;
+var y = 10;
+var z = x + y;
+console.log('The sum of ' + x + ' and ' + y + ' is ' + z);
+
+let name = 'John Doe';
+let age = 30;
+const pi = 3.14;
+
+console.log(name + ' is ' + age + ' years old');
+
+console.log('The value of pi is ' + pi);
