69d7bac2fac0074af30f920424cfd28b76a013a5
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-04-08 09:47:35 +0600

    operators

---
+++ ArithmeticOperators.js
@@ -0,0 +1,12 @@
+    let a=5;
+    let b=10;
+    let c=a+b;
+    console.log('The sum of ' + a + ' and ' + b + ' is ' + c);
+    let d=a-b;
+    console.log('The difference of ' + a + ' and ' + b + ' is ' + d);
+    let e=a*b;
+    console.log('The product of ' + a + ' and ' + b + ' is ' + e);  
+    let f=a/b;
+    console.log('The quotient of ' + a + ' and ' + b + ' is ' + f);
+    let g=a%b;
+    console.log('The remainder of ' + a + ' divided by ' + b + ' is ' + g);
